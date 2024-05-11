f57b4bef5505880cb46976fab641ef333fc96525
Jhony <Jhonny14_1@hotmail.com>
2024-05-11 18:09:45 -0500

    feat: task 3 - Printing teachers

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -9,27 +9,16 @@ interface Teacher {
   [key: string]: string | boolean | number | undefined;
 }
 
-// const teacher3: Teacher = {
-//   firstName: 'John',
-//   fullTimeEmployee: false,
-//   lastName: 'Doe',
-//   location: 'London',
-//   contract: false,
-// };
-
-// console.log(teacher3);
-
-// 2. Extending the Teacher class
-
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-console.log(director1);
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName.slice(0,1)}.${lastName}`;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+console.log(printTeacher('John', 'Doe'));
