d4157bd72507a04f9ab556bcec22b4d11d055a6b
Jhony <Jhonny14_1@hotmail.com>
2024-05-12 22:03:31 -0500

    feat: task 0 - Basic setup

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+const $ = require('jquery');
+
+// import $ from 'jquery';
+
+$(document).ready(function () {
+  $('body').append('<p>Holberton Dashboard 1</p>');
+  $('body').append('<p>Dashboard data for the students 2</p>');
+  $('body').append('<p>Copyright - Holberton School 3</p>');
+});
