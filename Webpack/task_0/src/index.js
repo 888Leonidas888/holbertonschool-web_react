d8531aa272bdaea5e5a233ffaac94a1c22cbc3eb
Jhony <Jhonny14_1@hotmail.com>
2024-05-12 22:07:48 -0500

    chore: actualiza.

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -3,7 +3,7 @@ const $ = require('jquery');
 // import $ from 'jquery';
 
 $(document).ready(function () {
-  $('body').append('<p>Holberton Dashboard 1</p>');
-  $('body').append('<p>Dashboard data for the students 2</p>');
-  $('body').append('<p>Copyright - Holberton School 3</p>');
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
 });
