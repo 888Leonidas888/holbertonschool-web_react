2f30bca356bd36739e598a03f9190d2474cd0a5a
Jhony <Jhonny14_1@hotmail.com>
2024-05-21 17:44:52 -0500

    chore: task 2 - se modifica importaciones.

--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -6,6 +6,3 @@ export function getFooterCopy(isIndex) {
   return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
 }
 
-export function getLatestNotification() {
-  return '<strong>Urgent requirement</strong> - complete by EOD';
-}
