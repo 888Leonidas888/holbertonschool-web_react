50ab7aa2751a241e5d112808688a156131c4b6ec
Jhony <Jhonny14_1@hotmail.com>
2024-05-24 18:37:46 -0500

    chore: modifica link e imagenes.

--- react_intro/task_4/src/App.js
+++ react_intro/task_4/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img
-          src="https://s3.eu-west-3.amazonaws.com/hbtn.intranet/uploads/medias/2019/11/175b85183ecedb529e14.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA4MYA5JM5DUTZGMZG%2F20240523%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20240523T214411Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=5440d7608343e72ad16ac5092c9585b1d72d1626e29033d658ced7b5182e5a45"
+          src="https://s3.eu-west-3.amazonaws.com/hbtn.intranet/uploads/medias/2019/11/175b85183ecedb529e14.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA4MYA5JM5DUTZGMZG%2F20240524%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20240524T201857Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=a689d1492dac8f73fa8a81d7e433cce504053ed18fb0f2e1de98189e0e2d3ffe"
           className="App-logo"
           alt="logo"
         />
