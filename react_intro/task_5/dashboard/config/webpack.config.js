67cdaf4f562bb4985c59cfe2a1c65490b524ddd5
Jhony <Jhonny14_1@hotmail.com>
2024-05-28 20:03:55 -0500

    feat: task 9 - Instalando babel.

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,45 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, '../dist'),
-    clean: true,
+    filename: 'bundle.js',
   },
-  mode: 'development',
   devtool: 'inline-source-map',
+  mode: 'development',
   devServer: {
-    static: path.resolve(__dirname, '../dist'),
     hot: true,
     open: true,
+    static: path.resolve(__dirname, '../dist'),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/favicon.ico', to: 'favicon.ico' },
+      ],
+    }),
+  ],
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              disble: true,
-            },
-          },
-        ],
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        loader: 'image-webpack-loader',
       },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      // template:path.resolve(__dirname,'../dist/index.html'),
-      inject: 'body'
-    }),
-  ],
 };
--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -1,6 +1,15 @@
-const p = document.createElement('p')
-p.textContent = `Me gustas cuando callas porque estás como ausente,
-y me oyes desde lejos, y mi voz no te toca.
-Parece que los ojos se te hubieran volado
-y parece que un beso te cerrara la boca.`
-document.body.appendChild(p)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Notifications from './Notifications';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <div className="root-notifications">
+      <Notifications />
+    </div>
+    <App />
+  </React.StrictMode>
+);
