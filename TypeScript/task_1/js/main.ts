d2697dd5ecc475ce744473403298d89642c65e5f
Jhony <Jhonny14_1@hotmail.com>
2024-06-09 17:41:11 -0500

    chore: task 4 - writing a class.

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -13,12 +13,44 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName.slice(0,1)}.${lastName}`;
-}
-
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-console.log(printTeacher('John', 'Doe'));
+const printTeacher: printTeacherFunction = function (
+  firstName: string,
+  lastName: string
+): string {
+  return `${firstName.slice(0, 1)}.${lastName}`;
+};
+
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass({ firstName: 'John', lastName: 'Doe' });
+console.log(student.displayName());
+console.log(student.workOnHomework());
