620848617cf605d2daebf69bbfad36efbe778fb0
Jhony <Jhonny14_1@hotmail.com>
2024-06-22 18:02:13 -0500

    chore: task 5 - react props - se agrega test en Notifications.test.js

--- react_props/task_5/dashboard/src/Notifications/Notifications.test.js
+++ react_props/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -7,7 +7,9 @@ describe('Notifications Component', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<Notifications displayDrawer />);
+    wrapper = shallow(
+      <Notifications displayDrawer={true} listNotifications={[]} />
+    );
   });
 
   it('Renderiza sin romperse.', () => {
@@ -18,8 +20,39 @@ describe('Notifications Component', () => {
     expect(wrapper.find(NotificationItem).length).toBe(0);
   });
 
-  // it('Renderiza con el texo "Here is the list of notifications"', () => {
-  //   // expect(wrapper.find('p').text()).toBe('Here is the list of notifications');
-  //   expect(wrapper.find('p').text()).toBe('Your notifications');
-  // });
+  it(
+    'Renderiza con el texo "No new notifications for now"' +
+      ' cuando displaDrawer=true y listNotifications=[]',
+    () => {
+      // expect(wrapper.find('p').text()).toBe('Here is the list of notifications');
+      expect(wrapper.find('.Notifications p').at(0).text()).toBe(
+        'No new notifications for now'
+      );
+    }
+  );
+  it(
+    'Renderiza con el texo "Here is the list of notifications" cuando displayDrawer=true' +
+      ' y listNotificatios se le pasa una lista con data.',
+    () => {
+      const notification = {
+        id: 2,
+        type: 'urgent',
+        value: 'New resume available',
+      };
+      wrapper = shallow(
+        <Notifications
+          displayDrawer={true}
+          listNotifications={[notification]}
+        />
+      );
+      expect(wrapper.find('.Notifications p').at(0).text()).toBe(
+        'Here is the list of notifications'
+      );
+    }
+  );
+
+  it('Renderiza con el texo "Your notifications" cuando displayDrawer=false', () => {
+    wrapper = shallow(<Notifications displayDrawer={false} />);
+    expect(wrapper.find('p').text()).toBe('Your notifications');
+  });
 });
