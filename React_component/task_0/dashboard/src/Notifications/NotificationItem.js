769c3b110feb697ae58e39d08aa51ad1d6725226
Jhony <Jhonny14_1@hotmail.com>
2024-06-23 20:22:36 -0500

    feat: react component - task 2 - crea un nuevo manejador de eventos. - envia a la consola notificición leída cuando se presione sobre una de ellas.

--- React_component/task_0/dashboard/src/Notifications/NotificationItem.js
+++ React_component/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 
-function NotificationItem({ type = 'default', html = null, value = '' }) {
+function NotificationItem({
+  type = 'default',
+  html = null,
+  value = '',
+  markAsRead,
+}) {
   if (html) {
     return (
-      <li data-notification-type={type} dangerouslySetInnerHTML={html}></li>
+      <li
+        data-notification-type={type}
+        dangerouslySetInnerHTML={html}
+        onClick={markAsRead}
+      ></li>
     );
   } else {
-    return <li data-notification-type={type}>{value}</li>;
+    return (
+      <li data-notification-type={type} onClick={markAsRead}>
+        {value}
+      </li>
+    );
   }
 }
 
