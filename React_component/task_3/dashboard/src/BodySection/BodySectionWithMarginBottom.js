3a3db91b20f59bfa5af266c4000f09ec6c055361
Jhony <Jhonny14_1@hotmail.com>
2024-06-24 17:01:03 -0500

    feat: react component - task 3 - especialización:     - se crea un nuevo componente BodySectionWithMarginBottom     - se crea hola de estilos para el nuevo componente.

---
+++ React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import BodySection from './BodySection';
+import './BodySectionWithMarginBottom.css';
+import PropTypes from 'prop-types';
+
+class BodySectionWithMarginBottom extends Component {
+  render() {
+    const { title, children = null } = this.props;
+    return (
+      <div className="bodySectionWithMargin">
+        <BodySection {...this.props} />
+      </div>
+    );
+  }
+}
+
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
+
+export default BodySectionWithMarginBottom;
